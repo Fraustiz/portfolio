38cc6990b3b038e81242c66fdf2114ed5cf5817e
Fraustiz <jules.drevon@gmail.com>
2024-06-03 17:56:30 +0200

    Added knowledge items

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Nav from "./components/Header/Nav/Nav"
 import Main from './components/Body/Main/Main'
 import About from './components/Body/About/About'
 import Interest from './components/Body/Interest/Interest'
+import Knowledge from './components/Body/Knowledge/Knowledge'
 import Studies from './components/Body/Studies/Studies'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Main/>
       <About/>
       <Interest/>
+      <Knowledge/>
       <Studies/>
     </>
   )
---
+++ src/components/Body/Knowledge/Knowledge.tsx
@@ -0,0 +1,49 @@
+import './Knowledge.css';
+
+export const Knowledge = () => {
+    return (
+        <div className="knowledge">
+            <div className="title">
+                <h1><i className="fa-solid fa-code"></i></h1>
+                <h2>connaissances</h2>
+            </div>
+            <div className="marquee">
+                <img className="marquee-item" src="./svgs/coding/html5.svg" alt="HTML5"/>
+                <img className="marquee-item" src="./svgs/coding/css.svg" alt="CSS"/>
+                <img className="marquee-item" src="./svgs/coding/bootstrap.svg" alt="Bootstrap"/>
+                <img className="marquee-item" src="./svgs/coding/discord.svg" alt="Discord"/>
+                <img className="marquee-item" src="./svgs/coding/djs.svg" alt="Discord.js"/>
+                <img className="marquee-item" src="./svgs/coding/figma.svg" alt="Figma"/>
+                <img className="marquee-item" src="./svgs/coding/git.svg" alt="Git"/>
+                <img className="marquee-item" src="./svgs/coding/github.svg" alt="GitHub"/>
+                <img className="marquee-item" src="./svgs/coding/javascript.svg" alt="JavaScript"/>
+                <img className="marquee-item" src="./svgs/coding/jquery.svg" alt="jQuery"/>
+                <img className="marquee-item" src="./svgs/coding/linux.svg" alt="Linux"/>
+                <img className="marquee-item" src="./svgs/coding/medusa.svg" alt="Medusa"/>
+                <img className="marquee-item" src="./svgs/coding/mysql.svg" alt="MySQL"/>
+                <img className="marquee-item" src="./svgs/coding/nextjs.svg" alt="Next.js"/>
+                <img className="marquee-item" src="./svgs/coding/nodejs.svg" alt="Node.js"/>
+                <img className="marquee-item" src="./svgs/coding/npm.svg" alt="NPM"/>
+                <img className="marquee-item" src="./svgs/coding/openai.svg" alt="OpenAI"/>
+                <img className="marquee-item" src="./svgs/coding/php.svg" alt="PHP"/>
+                <img className="marquee-item" src="./svgs/coding/postgresql.svg" alt="PostgreSQL"/>
+                <img className="marquee-item" src="./svgs/coding/prisma.svg" alt="Prisma"/>
+                <img className="marquee-item" src="./svgs/coding/react.svg" alt="React"/>
+                <img className="marquee-item" src="./svgs/coding/resend.svg" alt="Resend"/>
+                <img className="marquee-item" src="./svgs/coding/slack.svg" alt="Slack"/>
+                <img className="marquee-item" src="./svgs/coding/stackoverflow.svg" alt="Stack Overflow"/>
+                <img className="marquee-item" src="./svgs/coding/svgl.svg" alt="SVGL"/>
+                <img className="marquee-item" src="./svgs/coding/tailwindcss.svg" alt="Tailwind CSS"/>
+                <img className="marquee-item" src="./svgs/coding/typescript.svg" alt="TypeScript"/>
+                <img className="marquee-item" src="./svgs/coding/ubuntu.svg" alt="Ubuntu"/>
+                <img className="marquee-item" src="./svgs/coding/vercel.svg" alt="Vercel"/>
+                <img className="marquee-item" src="./svgs/coding/vitejs.svg" alt="ViteJS"/>
+                <img className="marquee-item" src="./svgs/coding/vscode.svg" alt="Visual Studio Code"/>
+                <img className="marquee-item" src="./svgs/coding/windows.svg" alt="Windows"/>
+                <img className="marquee-item" src="./svgs/coding/yarn.svg" alt="Yarn"/>
+            </div>
+        </div>
+    )
+}
+
+export default Knowledge;
