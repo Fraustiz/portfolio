25b929de6faba2c646c32d4c533198278069354d
Fraustiz <jules.drevon@gmail.com>
2024-06-06 20:30:13 +0200

    Nouvelle section

--- src/components/Body/Studies/Studies.tsx
+++ src/components/Body/Studies/Studies.tsx
@@ -2,11 +2,20 @@ import "./Studies.css";
 
 export const Studies = () => {
 
-  return (
-    <div className="studies" id="studies">
-
-    </div>
-  )
+    return (
+        <div className="studies">
+            <div className="title">
+                <h1><i className="fa-regular fa-school"></i></h1>
+                <h2>Mes études</h2>
+            </div>
+            <div className="studies-content">
+                <div className="study">
+                    <h3></h3>
+                    <p></p>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Studies;
--- src/components/ui/aurora-background.tsx
+++ src/components/ui/aurora-background.tsx
@@ -31,7 +31,7 @@ export const AuroraBackground = ({
               `
             [--white-gradient:repeating-linear-gradient(100deg,var(--white)_0%,var(--white)_7%,var(--transparent)_10%,var(--transparent)_12%,var(--white)_16%)]
             [--dark-gradient:repeating-linear-gradient(100deg,var(--black)_0%,var(--black)_7%,var(--transparent)_10%,var(--transparent)_12%,var(--black)_16%)]
-            [--aurora:repeating-linear-gradient(100deg,var(--blue-500)_10%,var(--indigo-300)_15%,var(--blue-300)_20%,var(--violet-200)_25%,var(--blue-400)_30%)]
+            [--aurora:repeating-linear-gradient(100deg,var(--lime-600)_10%,var(--lime-300)_15%,var(--green-200)_20%,var(--lime-100)_25%,var(--lime-400)_30%)]
             [background-image:var(--white-gradient),var(--aurora)]
             dark:[background-image:var(--dark-gradient),var(--aurora)]
             [background-size:300%,_200%]
