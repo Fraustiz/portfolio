159a048b492e8d3a6efb09e48b653ca159183cc9
Fraustiz <jules.drevon@gmail.com>
2024-06-07 18:31:07 +0200

    Ajout studies

--- src/components/Body/Studies/Studies.tsx
+++ src/components/Body/Studies/Studies.tsx
@@ -10,8 +10,55 @@ export const Studies = () => {
             </div>
             <div className="studies-content">
                 <div className="study">
-                    <h3></h3>
-                    <p></p>
+                    <div className="text">
+                        <div className="top-text">
+                            <h3>Lycée Pierre Mendes France</h3>
+                            <p><span>Diplôme :</span> Baccalauréat Système Numérique (Option RISC)</p>
+                            <p><span>Date :</span> Sept. 2018 <i className="fa-light fa-arrow-right-long"></i> Juin 2021</p>
+                        </div>
+                    </div>
+                    <div className="bottom-content">
+                        <div className="bottom-text">
+                            <p><span><i className="fa-regular fa-location-dot"></i></span> Veynes, France</p>
+                        </div>
+                        <div className="img">
+                            <img src="./images/veynes.jpg" alt="Lycée Pierre Mendes France" />
+                        </div>
+                    </div>
+                </div>
+                <div className="study">
+                    <div className="text">
+                        <div className="top-text">
+                            <h3>Formation OpenClassrooms</h3>
+                            <p><span>Description :</span> Développeur web et web mobile</p>
+                            <p><span>Date :</span> Fev. 2023 <i className="fa-light fa-arrow-right-long"></i> Oct. 2023</p>
+                        </div>
+                    </div>
+                    <div className="bottom-content">
+                        <div className="bottom-text">
+                            <p><span><i className="fa-regular fa-location-dot"></i></span> En ligne</p>
+                        </div>
+                        <div className="img">
+                            <img src="./images/openclassrooms.png" alt="OpenClassrooms" />
+                        </div>
+                    </div>
+                </div>
+                <div className="study">
+                    <div className="text">
+                        <div className="top-text">
+                            <h3>Institut G4</h3>
+                            <p><span>Description :</span> Développeur web et web mobile</p>
+                            <p><span>Date :</span> Fev. 2023 <i className="fa-light fa-arrow-right-long"></i> Oct. 2023</p>
+                        </div>
+                    </div>
+                    <div className="bottom-content">
+                        <div className="bottom-text">
+                            <p><span><i className="fa-regular fa-location-dot"></i></span> Lyon, France</p>
+                        </div>
+                        <div className="img">
+                            <img src="./images/openclassrooms.png" alt="OpenClassrooms" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
